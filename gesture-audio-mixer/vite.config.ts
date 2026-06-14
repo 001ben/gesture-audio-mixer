c4ab20fbc6e7f201201577390829e4cc0c827299
Ben Brown <001ben@live.com.au>
2026-06-14 21:49:35 +1000

    Add GitHub Pages deployment

--- gesture-audio-mixer/vite.config.ts
+++ gesture-audio-mixer/vite.config.ts
@@ -1,6 +1,9 @@
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS && repositoryName ? `/${repositoryName}/` : "/",
   plugins: process.env.VITE_USE_HTTPS === "1" ? [basicSsl()] : []
 });
